2d8c3c5501816799103d3f010cb7a66a98e973b3
Panos Matsinopoulos <panos@mixlr.com>
2023-06-06 17:07:39 +0300

    Display the albums inside a ScrollView

--- app/src/main/java/com/mixlr/panos/retrofitdemo/MainActivity.kt
+++ app/src/main/java/com/mixlr/panos/retrofitdemo/MainActivity.kt
@@ -5,12 +5,16 @@ import android.os.Bundle
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.liveData
+import com.mixlr.panos.retrofitdemo.databinding.ActivityMainBinding
 import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val retService = RetrofitInstance.getRetrofitInstance().create(AlbumService::class.java)
         val responseLiveData: LiveData<Response<Albums>> = liveData {
@@ -22,8 +26,11 @@ class MainActivity : AppCompatActivity() {
             if (albumsList != null) {
                 while (albumsList.hasNext()) {
                     val albumsItem = albumsList.next()
-
-                    Log.i("MYTAG", "Title: ${albumsItem.title}")
+                    val result = " " + "Album Title: ${albumsItem.title}" + "\n" +
+                                 " " + "Album id: ${albumsItem.id}" + "\n" +
+                                 " " + "User id: ${albumsItem.userId}" + "\n\n\n"
+                    Log.i("MYTAG", result)
+                    binding.textView.append(result)
                 }
             }
         }
