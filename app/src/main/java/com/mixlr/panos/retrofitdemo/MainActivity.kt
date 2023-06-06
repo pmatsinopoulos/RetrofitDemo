606199e7d7afa896a4713717b0186ede862b5c01
Panos Matsinopoulos <panos@mixlr.com>
2023-06-06 16:45:38 +0300

    Fetch the albums and log their title. All from MainActivity

--- app/src/main/java/com/mixlr/panos/retrofitdemo/MainActivity.kt
+++ app/src/main/java/com/mixlr/panos/retrofitdemo/MainActivity.kt
@@ -2,10 +2,30 @@ package com.mixlr.panos.retrofitdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.liveData
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val retService = RetrofitInstance.getRetrofitInstance().create(AlbumService::class.java)
+        val responseLiveData: LiveData<Response<Albums>> = liveData {
+            val response = retService.getAlbums()
+            emit(response)
+        }
+        responseLiveData.observe(this ) {
+            val albumsList = it.body()?.listIterator()
+            if (albumsList != null) {
+                while (albumsList.hasNext()) {
+                    val albumsItem = albumsList.next()
+
+                    Log.i("MYTAG", "Title: ${albumsItem.title}")
+                }
+            }
+        }
     }
 }
